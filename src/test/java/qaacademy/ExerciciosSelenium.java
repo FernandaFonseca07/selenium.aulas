8529afe2e0dcafadb1aa621134ca9ffbfd6a8cea
FernandaFonseca07 <fernanda_fonseca0107@hotmail.com>
2023-09-11 22:53:24 -0300

    Exercicios de seleção de itens/lista, melhorado com driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);

--- src/test/java/qaacademy/ExerciciosSelenium.java
+++ src/test/java/qaacademy/ExerciciosSelenium.java
@@ -1,7 +1,10 @@
 package qaacademy;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,11 +14,16 @@ import org.openqa.selenium.support.ui.Select;
 public class ExerciciosSelenium {
     static WebDriver driver;
 
+    @BeforeClass
+    public static void iniciaTeste() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://demo.automationtesting.in/Register.html");
+
+    }
+
     @Test
     public void exercicioFormulario() throws InterruptedException {
-        driver = new ChromeDriver();
-        driver.get("https://demo.automationtesting.in/Register.html");
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@placeholder='First Name']")).sendKeys("Fernanda");
         driver.findElement(By.xpath("//*[@placeholder='Last Name']")).sendKeys("Fonseca");
         driver.findElement(By.xpath("//*[@id='basicBootstrapForm']/div[2]/div/textarea")).sendKeys("Brazil");
@@ -23,27 +31,29 @@ public class ExerciciosSelenium {
         driver.findElement(By.xpath("//*[@ng-model='Phone']")).sendKeys("1234567890");
 
         // Selecionando os boxes de opções
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@value='FeMale']")).click();
-        ;
         Assert.assertTrue(driver.findElement(By.xpath("//*[@value='FeMale']")).isSelected());
 
         driver.findElement(By.xpath("//*[@value='Movies']")).click();
         Assert.assertTrue(driver.findElement(By.xpath("//*[@value='Movies']")).isSelected());
 
         // // language button
-        driver.findElement(By.xpath("//div[@id='msdd' and @class='ui-autocomplete-multiselect ui-state-default ui-widget']")).click();
+        driver.findElement(
+                By.xpath("//div[@id='msdd' and @class='ui-autocomplete-multiselect ui-state-default ui-widget']"))
+                .click();
 
-        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(text(),'English')]")).click();
         driver.findElement(By.xpath("//a[contains(text(),'Portuguese')]")).click();
 
-        Thread.sleep(2000);
-
-        boolean linguagemPortugueseSelecionada = driver.findElement(By.xpath("//div[contains(text(), 'Portuguese') and @class='ui-autocomplete-multiselect-item']")).isDisplayed();
-        boolean linguagemEnglishSelecionada = driver.findElement(By.xpath("//div[contains(text(), 'English') and @class='ui-autocomplete-multiselect-item']")).isDisplayed();
+        boolean linguagemPortugueseSelecionada = driver
+                .findElement(
+                        By.xpath("//div[contains(text(), 'Portuguese') and @class='ui-autocomplete-multiselect-item']"))
+                .isDisplayed();
+        boolean linguagemEnglishSelecionada = driver
+                .findElement(
+                        By.xpath("//div[contains(text(), 'English') and @class='ui-autocomplete-multiselect-item']"))
+                .isDisplayed();
         Assert.assertTrue(linguagemPortugueseSelecionada && linguagemEnglishSelecionada);
-
 
         // Skills com select
         // WebElement selectElement = driver.findElement
@@ -53,26 +63,23 @@ public class ExerciciosSelenium {
 
         // Country
         driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[10]/div/span/span[1]/span")).click();
-        Thread.sleep(3000);
         driver.findElement(By.xpath("//li[contains(text(),'Japan')]")).click();
-        Assert.assertTrue(driver.findElement(By.xpath("//span[contains(text(), 'Japan') and @id='select2-country-container']")).isDisplayed());
-
+        Assert.assertTrue(
+                driver.findElement(By.xpath("//span[contains(text(), 'Japan') and @id='select2-country-container']"))
+                        .isDisplayed());
 
         // Year
         driver.findElement(By.xpath("//*[@placeholder='Year']")).click();
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@value='1997']")).click();
         Assert.assertTrue(driver.findElement(By.xpath("//*[@value='1997']")).isSelected());
 
         // Month
         driver.findElement(By.xpath("//*[@placeholder='Month']")).click();
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@value='April']")).click();
         Assert.assertTrue(driver.findElement(By.xpath("//*[@value='April']")).isSelected());
 
         // Day
         driver.findElement(By.xpath("//*[@placeholder='Day']")).click();
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@value='1']")).click();
         Assert.assertTrue(driver.findElement(By.xpath("//*[@value='1']")).isSelected());
 
@@ -82,6 +89,9 @@ public class ExerciciosSelenium {
 
         // Submit
         driver.findElement(By.xpath("//*[@id='submitbtn']")).click();
+
+        String msgErroTelefone = "Portuguese";
+        Assert.assertTrue("Linguagem", driver.getPageSource().contains(msgErroTelefone));
 
     }
 
